4b13a5359b171f05ae506ab1cd3df0a8f5e91b99
Richard Wang <richardwang981@yahoo.com>
2021-10-31 03:03:51 -0500

    CSS working for place votes
    Need to work on dragging using panresponder

--- consilia/pages/PlaceVotes.js
+++ consilia/pages/PlaceVotes.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, FlatList, TextInput, Image } from 'react-native';
+import { Animated, PanResponder, View, Text, TouchableOpacity, StyleSheet, ScrollView, FlatList, TextInput, Image, Dimensions } from 'react-native';
 import EventType from '../components/EventType';
 import TransportMode from '../components/TransportMode';
+import PlaceData from '../components/PlaceData';
+import { tsConstructorType } from '@babel/types';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREENWIDTH = Dimensions.get('window').width;
+const data = [
+    {
+        "PlaceID" : "1",
+        "EventID" : "ajdfln1-2341",
+        "Rating" : 3.8,
+        "Name" : "ACL",
+        "Description" : "Music festival to get high at",
+        "Longitude" : -92.001,
+        "Latitude" : 30.43,
+        "ImageURL" : "https://edm.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTczNjY4MTk3OTU2ODU1NzE3/austin-city-limits.jpg",
+        "GooglePlaceID" : "lksdjfano32",
+    },
+    {
+        "PlaceID" : "2",
+        "EventID" : "sdfnmkglfer",
+        "Rating" : 4.0,
+        "Name" : "Fuddruckers",
+        "Description" : "Burger place that fudds",
+        "Longitude" : -92.3201,
+        "Latitude" : 30.42,
+        "ImageURL" : "https://3e87eb59177583ca20e5-3c4f8e07d4ab2f5f48a61d1d9b0d1b8c.ssl.cf2.rackcdn.com/20161012095655-f59db4bc.jpg",
+        "GooglePlaceID" : "lksdjfano32",
+    }
+]
+
 
 export default function PlaceVotes(props) {
-  return (
+
+    var renderPlaces = () =>{
+        return data.map((item,i) =>{
+            return(
+                <Animated.View key={item.PlaceID} style={styles.animated}>
+                    <Text style={styles.titleText}>{item.Name}</Text>
+                    <Text style={styles.desc}>{item.Description}</Text>
+                    <Image style={styles.img} 
+                    source={{uri: item.ImageURL}}></Image>
+                    <Text>{item.Rating} stars</Text>
+                </Animated.View>
+            )
+        })
+    }
+  
+    return (
     <View style={styles.container}>
-      <TextInput style={styles.linkStyle}>{props.link}</TextInput>
-      <Text style={styles.titleText}>{props.title}</Text>
-      <Image style={styles.img} source={
-        {uri: "https://edm.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTczNjY4MTk3OTU2ODU1NzE3/austin-city-limits.jpg"}
-      }></Image>
-      <Text>{props.distance} away</Text>
-      <Text>{props.rating} stars</Text>
+    
+      {renderPlaces()}
 
       <TouchableOpacity onPress={props.goHome}>
         <Text>Return Home</Text>
@@ -22,15 +62,41 @@ export default function PlaceVotes(props) {
 }
 
 const styles = StyleSheet.create({
+  animated:{
+    height:SCREEN_HEIGHT-120,
+    width:SCREENWIDTH,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
   container: {
     height: '100%',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    flex: 1
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  card:{
+      flex: 0.45,
+      borderRadius: 8,
+      shadowRadius:25,
+      shadowColor: '#000',
+      shadowOpacity: 0.08,
+      shadowOffset: {width:0, height:0},
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'fff'
+
+  },
+  cardImage: {
+      width: 160,
+      flex: 1,
+      resizeMode: 'contain'
   },
   img: {
-    width: "95%",
-    height: 500
+      flex:1,
+      width: "95%",
+      height: 500,
+      borderRadius:20
   },
   linkStyle: {
     backgroundColor: "white",
@@ -44,5 +110,8 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 30,
     marginBottom: 10,
+  },
+  desc: {
+      fontSize: 16
   }
 })
