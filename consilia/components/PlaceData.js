6271bfb36eb7e1649644cec620a28e74216d87e5
Richard Wang <richardwang981@yahoo.com>
2021-10-31 01:11:36 -0500

    Create PlaceData.js
    has temp place data for tinder swipe cards

---
+++ consilia/components/PlaceData.js
@@ -0,0 +1,24 @@
+export default [
+    {
+        "PlaceID" : "1",
+        "EventID" : "ajdfln1-2341",
+        "Rating" : 3.8,
+        "Name" : "ACL",
+        "Description" : "Music festival to get high at",
+        "Longitude" : -92.001,
+        "Latitude" : 30.43,
+        "Image" : "https://edm.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTczNjY4MTk3OTU2ODU1NzE3/austin-city-limits.jpg",
+        "GooglePlaceID" : "lksdjfano32",
+    },
+    {
+        "PlaceID" : "2",
+        "EventID" : "sdfnmkglfer",
+        "Rating" : 4.0,
+        "Name" : "Fuddruckers",
+        "Description" : "Burger place that fudds",
+        "Longitude" : -92.3201,
+        "Latitude" : 30.42,
+        "Image" : "https://3e87eb59177583ca20e5-3c4f8e07d4ab2f5f48a61d1d9b0d1b8c.ssl.cf2.rackcdn.com/20161012095655-f59db4bc.jpg",
+        "GooglePlaceID" : "lksdjfano32",
+    }
+]
